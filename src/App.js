a172f9d7b717e078ab40fd1f50eb82f66f6c2e9c
Lucero07 <ing.lucero.ramos@gmail.com>
2017-08-15 18:11:13 -0500

    Componentes de Comentaarios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import TarjetaComentario from './components/TarjetaComentario';
+import ContenedorComentario from './containers/ContenedorComentario';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-        
+
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ContenedorComentario />
       </div>
     );
   }
