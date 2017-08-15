3f7db67e3234c88bbaa490fbe4c1334d41d22dc0
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-08-15 17:54:22 -0500

    Agregando barra de navegación lateral derecha, para mostrar en el home

---
+++ src/components/NavUsuarios.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../css/NavUsuarios.css"
+
+export default class NavUsuarios extends React.Component {
+  render(){
+    return(
+      <div id="sidebar" role="navigation" className="col m2 s2 offset-m10 offset-s10">
+        <div id="sidebar-nav">
+          <ul className="nav nav-stacked">
+            <li className="bordeUsuario">
+              <a>
+                <img src="https://dummyimage.com/100x100/#fff/00ff5e.jpg" className="responsive-img radius" alt="fotoUsuario"/>
+                <p>Nombre Usuario</p>
+              </a>
+            </li>
+            <li className="bordeUsuario">
+              <a>
+                <img src="https://dummyimage.com/100x100/#fff/00ff5e.jpg" className="responsive-img radius" alt="fotoUsuario"/>
+                <p>Nombre Usuario</p>
+              </a>
+            </li>
+            <li className="bordeUsuario">
+              <a>
+                <img src="https://dummyimage.com/100x100/#fff/00ff5e.jpg" className="responsive-img radius" alt="fotoUsuario"/>
+                <p>Nombre Usuario</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
