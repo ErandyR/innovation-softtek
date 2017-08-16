670af485d6a5be57df887c3042dfdc87162c28ea
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-15 23:56:52 -0500

    cambios tarjeta

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -20,21 +20,39 @@ export default class Home extends Component {
             <NavPublicacion/>
           </Col>
           <Col m={10} className='col-margin'>
-            <Row>
-              <Col m={4}></Col>
-              <Col m={4}></Col>
-              <Col m={4}></Col>
-            </Row>
-            <Row>
               <Col m={4}>
-                <TarjetaPublicacion
-                  titulo='Optimizacion'
-                  tipo='Idea' background='rgb(71, 168, 199)'
-                  time='13 días'
-                  location='GDC Monterrey'
-                  descripcion='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et fuga accusantium maxime.'/>
+                <Col m={12}>
+                  <h3 className='center'>Recientes</h3>
+                  <TarjetaPublicacion
+                    titulo='Optimizacion'
+                    tipo='Idea' background='rgb(71, 168, 199)'
+                    time='13 días'
+                    location='GDC Monterrey'
+                    descripcion='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et fuga accusantium maxime.'/>
+                </Col>
               </Col>
-            </Row>
+              <Col m={4}>
+                <Col m={12}>
+                  <h3 className='center'> Populares</h3>
+                  <TarjetaPublicacion
+                    titulo='Optimizacion'
+                    tipo='Idea' background='rgb(71, 168, 199)'
+                    time='13 días'
+                    location='GDC Monterrey'
+                    descripcion='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et fuga accusantium maxime.'/>
+                </Col>
+              </Col>
+              <Col m={4}>
+                <Col m={12}>
+                  <h3 className='center'>Sugerencias</h3>
+                  <TarjetaPublicacion
+                    titulo='Optimizacion'
+                    tipo='Idea' background='rgb(71, 168, 199)'
+                    time='13 días'
+                    location='GDC Monterrey'
+                    descripcion='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et fuga accusantium maxime.'/>
+                </Col>
+              </Col>
           </Col>
           <Col m={1}>
             <NavUsuarios/>
