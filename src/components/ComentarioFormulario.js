f108c3c9499d5bf24573c1d217b504487f9c1983
Lucero07 <ing.lucero.ramos@gmail.com>
2017-08-15 23:11:27 -0500

    pagina Reto input sin margen

--- src/components/ComentarioFormulario.js
+++ src/components/ComentarioFormulario.js
@@ -1,12 +1,13 @@
 import React,{Component} from 'react';
 import {Col,Row,Button,Input,Icon} from 'react-materialize';
+import '../css/Comentarios.css';
 
 export default class ComentarioFormulario extends Component{
   render(){
     return(
       <Row>
 
-          <Input placeholder="Comentario" s={12}  />
+          <Input id="input"placeholder="Comentario" s={12}  />
           <Button waves='light'>Agregar<Icon left>comment</Icon></Button>
       </ Row>
 
