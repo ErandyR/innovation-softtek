dec0c7e4fdeebc017de21e54c48bf9045002e0e8
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-16 10:11:52 -0500

    flujo a pag usuario y a pag tag

--- src/components/NavPrincipal.js
+++ src/components/NavPrincipal.js
@@ -37,7 +37,7 @@ export default class NavPrincipal extends Component {
             <i className='material-icons icon'>notifications</i>
           </Col>
           <Col s={1}>
-            <img className='foto-perfil responsive-img' src="https://www.1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png" alt=""/>
+            <Link to='/paginaUsuario'><img className='foto-perfil responsive-img' src="https://www.1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png" alt=""/></Link>
           </Col>
         </Row>
       </nav>
