708f6e559776ee722d88ccda3b1066e3d828d971
ErandyR <rerandy15@gmail.com>
2017-08-15 20:51:19 -0500

    maquetacion tarjeta de contribucion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import App from './App';
+import AgregarContribucion from './components/AgregarContribucion';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <AgregarContribucion />
       </div>
     );
   }
