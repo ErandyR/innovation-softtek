2b7746b45710823b2359d48bced5dd63eaacd0db
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-16 00:41:30 -0500

    redireccion a navdetalle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Home from './containers/Home';
+import NavDetalle from './components/NavDetalle';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
-
+        <Router>
+      <div>
+        <Route exact path='/home' component={Home}/>
+        <Route  path='/detalle' component={NavDetalle}/>
+      </div>
+    </Router>
       </div>
     )
   }
