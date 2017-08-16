2a1aafef33116988a65adf40800f7a68c22869d6
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-08-15 19:41:24 -0500

    Corrigiendo errores de la barra de navegacion lateral derecha para poder visualizar con demás componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import NavUsuarios from "./components/NavUsuarios"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <NavUsuarios />
       </div>
     );
   }
