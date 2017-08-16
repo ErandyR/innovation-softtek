d4769b164f7a651c2deddafb004c4a1b0865625a
ErandyR <rerandy15@gmail.com>
2017-08-16 00:36:25 -0500

    pagina detalle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import PaginaDetalle from './containers/PaginaDetalle';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <PaginaDetalle />
       </div>
     );
   }
---
+++ src/components/ContenidoDetalle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Col, Row } from 'react-materialize';
+
+export default class ContenidoDetalle extends Component {
+    render(){
+        return(
+                <div>
+                    holaMundo
+                </div>
+        )
+    }
+}
